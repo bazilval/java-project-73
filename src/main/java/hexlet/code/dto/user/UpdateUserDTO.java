26a5cf0e40cd5ca254075264777f69bd8bd99fc1
Valentin Bazilevich <bazilvalentin@gmail.com>
2023-09-21 02:28:43 +0200

    Fix updateDTO validation

--- src/main/java/hexlet/code/dto/user/UpdateUserDTO.java
+++ src/main/java/hexlet/code/dto/user/UpdateUserDTO.java
@@ -1,5 +1,6 @@
 package hexlet.code.dto.user;
 
+import jakarta.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.Email;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,15 +10,19 @@ import org.openapitools.jackson.nullable.JsonNullable;
 @Getter
 @Setter
 public class UpdateUserDTO {
+    @NotNull
     @Length(min = 1, message = "Firstname has to contain at least 1 symbol")
     private JsonNullable<String> firstName;
 
+    @NotNull
     @Length(min = 1, message = "Lastname has to contain at least 1 symbol")
     private JsonNullable<String> lastName;
 
+    @NotNull
     @Email(message = "Email has to be correct")
     private JsonNullable<String> email;
 
+    @NotNull
     @Length(min = 3, message = "Password has to contain at least 3 symbols")
     private JsonNullable<String> password;
 }
